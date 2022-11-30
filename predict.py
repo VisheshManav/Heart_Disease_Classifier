454df4c377f1b2494b7050645c8aefdaf357f36d
Vishesh Manav <xvishesh77@gmail.com>
2022-11-30 23:34:27 +0530

    created html templates and likewise changes to predict.py

--- predict.py
+++ predict.py
@@ -1,7 +1,5 @@
 import pickle
-from flask import Flask
-from flask import request
-from flask import jsonify
+from flask import Flask, render_template, request
 
 model_file = 'model_lr.bin'
 
@@ -10,20 +8,34 @@ with open(model_file, 'rb') as f_in:
 
 app = Flask('heartD')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/')
+def home_form():
+    return render_template('home.html')
+
+@app.route('/', methods=['POST', 'GET'])
 def predict():
-    patient = request.get_json()
+
+    patient = {
+        "age": request.form['age'],
+        "sex": request.form['sex'], 
+        "cp": request.form['cp'],
+        "trestbps": request.form['trestbps'],
+        "chol": request.form['chol'],
+        "fbs": request.form['fbs'],
+        "restecg": request.form['restecg'],
+        "thalach": request.form['thalach'],
+        "exang": request.form['exang'],
+        "oldpeak": request.form['oldpeak'],
+        "slope": request.form['slope'],
+        "ca": request.form['ca'],
+        "thal": request.form['thal']
+    }
 
     X = dv.transform([patient])
     y_pred = model.predict_proba(X)[0, 1]
-    disease = y_pred > 0.5
+    disease_status = y_pred > 0.5
 
-    result = {
-        'disease_probability': float(y_pred),
-        'disease': bool(disease)
-    }
-
-    return jsonify(result)
+    return render_template('prediction.html', pred=y_pred, disease=disease_status)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9696)
